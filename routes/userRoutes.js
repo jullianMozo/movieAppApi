4de89355a2fbea315a8597b9e568d52c91d57807
jullian.mozo <jullian.mozo@gmail.com>
2024-08-30 09:40:24 +0800

    s83 update

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controller/userController');
+const { verify, verifyAdmin, isLoggedIn } = require("../auth");
 
 router.post('/register', userController.register);
 router.post('/login', userController.login);
 
 // Route for retreiving user details
 router.get("/details", verify, userController.retrieveUserDetails);
-
 
 module.exports = router;
