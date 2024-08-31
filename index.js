e8a933e44b805e0f7df7834baa6f9407888a0abd
jullian.mozo <jullian.mozo@gmail.com>
2024-08-31 10:55:20 +0800

    s83 update index corsoption

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(errorHandler);
 const corsOptions = {
     //client/Frontend application URL
     // Allow requests from this origin (The client's URL) the origin is in array form if there are multiple origins.
-    origin: ['http://localhost:3000'],
+    origin: ['http://localhost:3000','https://movie-app-client-psi.vercel.app'],
     // Allow only specified headers // optional only if you want to restrict the headers
     //allowedHeaders: ['Content-Type', 'Authorization'], 
     credentials: true,
